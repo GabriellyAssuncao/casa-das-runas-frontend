ba501c1cf6a1d8e6941704446ccf849c25e3e647
Nathan <nathan11.abreu@gmail.com>
2023-05-26 22:52:25 -0300

    Feature: Fixing Bugs

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,6 @@
 import * as C from './style.js'
 import logo from '../../img/logo.png'
 import nome from '../../img/nome.png'
-import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
@@ -14,7 +13,7 @@ const Header = () => {
             <C.Navigation>
                 <li><a href='#'>Poções</a></li>
                 <li><a href='#'>Feitiços</a></li>
-                <li><Link to='Login'><a>Conta</a></Link></li>
+                <li><a>Conta</a></li>
             </C.Navigation>
         </C.HeaderStyle>
     );
